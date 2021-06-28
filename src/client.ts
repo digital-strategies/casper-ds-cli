a2117072fb9dcccdb60267c979df7e40fd35535e
Marcin <zolkiewski.marcin@gmail.com>
2021-06-28 14:02:52 +0200

    refactor and pkg config

--- src/client.ts
+++ src/client.ts
@@ -1,16 +1,4 @@
 import { CasperClient, Keys, CasperServiceByJsonRPC } from 'casper-client-sdk';
-// import { CASPER_NODE_URL } from './env';
-
-// const casperPubPath = './casper_public.pem';
-// const casperPkPath = './casper_private.pem';
-
-// export const client = new CasperClient(CASPER_NODE_URL);
-
-// export const jsonRpcClient = new CasperServiceByJsonRPC(CASPER_NODE_URL);
-
-// export const keypair = Keys.Ed25519.parseKeyFiles(casperPubPath, casperPkPath);
-
-// console.info(`Casper wallet is ${keypair.accountHex()}`);
 
 export function readKeypair(pubPath: string, privPath: string) {
   const keypair = Keys.Ed25519.parseKeyFiles(pubPath, privPath);
